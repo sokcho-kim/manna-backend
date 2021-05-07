d9e9dea8d71b003c89ccf423754cd742dbaa0bc5
gunkims <45007556+kanpann@users.noreply.github.com>
2021-05-07 19:53:36 +0900

    프로젝트 생성

---
+++ src/test/java/com/manna/app/MannaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manna.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MannaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
